fb66d9232e659d6035ef1c2bc5f9a591015ff742
Justina <justina.saccomani@ucalgary.ca>
2022-11-27 05:59:52 +0000

    Expenses scene and buttons between expenses scene and main scene

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,6 +16,9 @@ public class Main extends Application {
 			FXMLLoader loader = new FXMLLoader();
 			VBox root = loader.load(new FileInputStream("src/application/ServiceSheetsView.fxml"));
 			
+			ServiceSheetsController controller = (ServiceSheetsController)loader.getController();
+			controller.applicationStage = primaryStage;
+			
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Manage Spreadsheets");
--- src/application/ServiceSheetsController.java
+++ src/application/ServiceSheetsController.java
@@ -2,14 +2,42 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
 
 public class ServiceSheetsController {
+	
+	Stage applicationStage;
 	
 	@FXML
     void enterEarnings (ActionEvent enterEarningsEvent) {}
 	
 	@FXML
     void enterExpenses (ActionEvent enterExpensesEvent) {
+		Scene mainScene = applicationStage.getScene();
+		applicationStage.setTitle("Enter expenses");
+		
+		VBox expensesBox = new VBox();
+    	
+    	Label titleLabel = new Label("Enter expenses");
+    	
+    	Button doneButton = new Button("Done");
+    	doneButton.setOnAction(doneEvent -> addExpenses(mainScene));
+  
+    	expensesBox.getChildren().addAll(titleLabel, doneButton);
+    	Scene expensesScene = new Scene(expensesBox,400,400);
+    	
+    	applicationStage.setScene(expensesScene);
 	}
-
+	
+	@FXML
+	void addExpenses(Scene mainScene) {
+		
+		applicationStage.setScene(mainScene);
+    	applicationStage.setTitle("Expenses Added");
+	}
+	
 }
